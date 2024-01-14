efee56fb272610a35d68d7aeac7e47aef68da80d
skuznecov9 <s.kuznecov@danaflex.ru>
2024-01-14 09:53:58 +0300

    npm and webpack

---
+++ webpack.config.cjs
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js',
+        clean: true
+    },
+    mode: 'production',
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: './index.html',
+            template: './src/index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    devServer: {
+        open: true,
+        liveReload: true,
+        static: path.resolve(__dirname, './src'),
+
+    },
+    target: 'web',
+}
