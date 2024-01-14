52f9d8c680a451ab47538fa2d324a778363ddfa0
smiths-sergey <153411565+smiths-sergey@users.noreply.github.com>
2024-01-14 08:56:20 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,4 @@
+export const name = 'Sergey';
+export const age=34;
+const email = 's.kuznecov@danaflex.ru';
+export default email;
---
+++ script2.js
@@ -0,0 +1,7 @@
+import {name} from './script1.js';
+import {age} from './script1.js';
+import email from './script1.js';
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Email: ${email}`);
